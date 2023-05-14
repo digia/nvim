52aeef8ae3c606c784205b6fb2a4f1761819c3ca
digia <jon@digia.io>
2023-05-14 17:13:17 -0400

    add phpactor and lua_ls LSP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -93,6 +93,29 @@ lspconfig.bashls.setup(lsp_config())
 lspconfig.jsonls.setup(lsp_config())
 lspconfig.dockerls.setup(lsp_config())
 lspconfig.jdtls.setup(lsp_config())
+lspconfig.phpactor.setup(lsp_config())
+lspconfig.lua_ls.setup(lsp_config({
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}))
 
 -- lspconfig.intelephense.setup{}
 -- lspconfig.vimls.setup{}
