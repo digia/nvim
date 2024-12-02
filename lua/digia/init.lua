407d765904628b440f7800a28f5c7c30eaf6ca31
digia <jon@digia.io>
2024-12-02 02:13:02 -0500

    feat: add init. autocmd by filetype

--- lua/digia/init.lua
+++ lua/digia/init.lua
@@ -11,7 +11,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -22,60 +22,88 @@ vim.opt.rtp:prepend(lazypath)
 
 -- https://lazy.folke.io/configuration
 require("lazy").setup({
-    spec = {
-        { import = "digia.plugin" },
-    },
-    -- colorscheme that will be used when installing plugins.
-    -- install = { colorscheme = { "solarized8" } },
+  spec = {
+    { import = "digia.plugin" },
+  },
+  -- colorscheme that will be used when installing plugins.
+  -- install = { colorscheme = { "solarized8" } },
 
-    -- automatically check for plugin updates
-    checker = {
-        enabled = true,
-        notify = false,
-    },
+  -- automatically check for plugin updates
+  checker = {
+    enabled = true,
+    notify = false,
+  },
 
-    change_detection = {
-        notify = false,
-    },
+  change_detection = {
+    notify = false,
+  },
 })
 
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 local DigiaGroup = augroup("Digia", {})
--- local HighlightYankGroup = augroup("HighlightYank", {})
 
-
-autocmd({"BufWritePre"}, {
-    group = DigiaGroup,
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
+autocmd({ "BufWritePre" }, {
+  group = DigiaGroup,
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
 })
 
 autocmd("BufEnter", {
-    group = DigiaGroup,
-    callback = function()
-        -- Being the primary color scheme it's loaded within the plugin spec
-        -- vim.cmd.colorscheme("solarized8_flat")
-    end
+  group = DigiaGroup,
+  callback = function()
+    -- Being the primary color scheme it's loaded within the plugin spec
+    -- vim.cmd.colorscheme("solarized8_flat")
+  end
 })
 
+autocmd({ "BufNewFile", "BufRead" }, {
+  group = DigiaGroup,
+  pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" },
+  callback = function()
+    vim.opt.tabstop = 2
+    vim.opt.softtabstop = 2
+    vim.opt.shiftwidth = 2
+  end
+}) -- JavaScript, typescript, tsx formatting
+
+autocmd({ "BufNewFile", "BufRead" }, {
+  group = DigiaGroup,
+  pattern = { "*.html", "*.css", "*.sass", "*.scss", "*.json", "*.jsonc" }, -- Should probably add all the other possibilities here...
+  callback = function()
+    vim.opt.tabstop = 2
+    vim.opt.softtabstop = 2
+    vim.opt.shiftwidth = 2
+  end
+}) -- HTML, CSS, etc. formatting
+
+autocmd({ "BufNewFile", "BufRead" }, {
+  group = DigiaGroup,
+  pattern = { "*.lua" },
+  callback = function()
+    vim.opt.tabstop = 2
+    vim.opt.softtabstop = 2
+    vim.opt.shiftwidth = 2
+  end
+}) -- Lua formatting
+
 -- autocmd("LspAttach", {
-    -- group = DigiaGroup,
-    -- callback = function(e)
-        -- local opts = { buffer = e.buf }
-        -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        -- -- Using "v" prefix due to it being a "visual" action -- e.g. it opens a split pane
-        -- -- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        -- vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        -- vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-        -- vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-        -- vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-        -- vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-        -- vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-        -- vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-        -- vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-        -- vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-        -- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
-    -- end
+-- group = DigiaGroup,
+-- callback = function(e)
+-- local opts = { buffer = e.buf }
+-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+-- -- Using "v" prefix due to it being a "visual" action -- e.g. it opens a split pane
+-- -- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+-- vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+-- vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+-- vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+-- vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+-- vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+-- vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+-- vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+-- vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+-- vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+-- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
+-- end
 -- })
