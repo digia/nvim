1979f496191e12bf311c8937c6daaa8a0b9ec14a
digia <jon@digia.io>
2024-12-04 00:34:23 -0500

    chore: move goto-preview to coding module

--- lua/digia/plugin/coding.lua
+++ lua/digia/plugin/coding.lua
@@ -164,6 +164,65 @@ return {
 
   },
 
+  -- TODO: Configure goto-preview (https://github.com/rmagatti/goto-preview)
+  {
+    "rmagatti/goto-preview",
+    lazy = true,
+    event = "VeryLazy",
+
+    -- keys = {
+    --   { "<C-h>", "<C-w>w", },
+    --   {
+    --     "P",
+    --     function()
+    --       require("goto-preview").goto_preview_definition({ focus_on_open = false, dismiss_on_move = true })
+    --     end,
+    --     { noremap = true, desc = "Peek Definition" },
+    --   },
+    -- },
+
+    opts = function()
+      -- Dev's configurations: https://github.com/rmagatti/dotfiles/blob/master/nvim/lua/rmagatti/goto-preview.lua
+      local preview_mapping = function(wincmd_direction, bufnr)
+        local function close()
+          vim.cmd("wincmd " .. wincmd_direction)
+          require("goto-preview").close_all_win({ skip_curr_window = true })
+          -- vim.lsp.buf.definition()
+        end
+
+        vim.keymap.set("n", "<C-w>" .. wincmd_direction, close, {
+          noremap = true,
+          silent = true,
+          buffer = bufnr,
+        })
+      end
+
+      -- Mapping to cycle between windows
+      -- vim.keymap.set("n", "<C-h>", "<C-w>w")
+
+      -- "Peek" mapping
+      -- vim.keymap.set(
+      --   "n",
+      --   "L", -- for "look"
+      --   function()
+      --     require("goto-preview").goto_preview_definition { focus_on_open = false, dismiss_on_move = true }
+      --   end,
+      --   { noremap = true }
+      -- )
+
+      return {
+        default_mappings = true,
+        resizing_mappings = true,
+        post_open_hook = function(bufnr)
+          preview_mapping("H", bufnr)
+          preview_mapping("J", bufnr)
+          preview_mapping("K", bufnr)
+          preview_mapping("L", bufnr)
+        end,
+      }
+    end,
+  },
+
   -- Autopairing
   {
     "windwp/nvim-autopairs",
