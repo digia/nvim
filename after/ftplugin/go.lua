200c3312d64d96463443f8ad0786a3a081642078
digia <jon@digia.io>
2025-11-01 17:37:04 -0400

    feat: add Go lang configs

---
+++ after/ftplugin/go.lua
@@ -0,0 +1,48 @@
+-- Go-specific settings
+local opt = vim.opt_local
+
+-- Indentation: use tabs, not spaces
+opt.expandtab = false
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 0
+
+-- Display tabs as 4 spaces visually
+opt.listchars = "tab:  ,extends:›,precedes:‹,nbsp:·,trail:·"
+
+-- Format on save using LSP (gopls handles gofmt/goimports)
+vim.api.nvim_create_autocmd("BufWritePre", {
+    buffer = 0,
+    callback = function()
+        -- Format with LSP
+        vim.lsp.buf.format({ async = false })
+
+        -- Organize imports
+        local params = vim.lsp.util.make_range_params()
+        params.context = { only = { "source.organizeImports" } }
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+        for cid, res in pairs(result or {}) do
+            for _, r in pairs(res.result or {}) do
+                if r.edit then
+                    local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                    vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                end
+            end
+        end
+    end
+})
+
+-- Toggle visible tabs
+vim.keymap.set("n", "<leader>vt", function()
+    if vim.opt_local.listchars:get().tab == "  " then
+        vim.opt_local.listchars = "tab:│ ,extends:›,precedes:‹,nbsp:·,trail:·"
+        print("Tabs visible")
+    else
+        vim.opt_local.listchars = "tab:  ,extends:›,precedes:‹,nbsp:·,trail:·"
+        print("Tabs hidden")
+    end
+end, { buffer = 0, desc = "Toggle visible tabs" })
+
+-- Go-specific keybindings
+vim.keymap.set("n", "<leader>gt", "<cmd>GoTest<cr>", { buffer = 0, desc = "Run Go tests" })
+vim.keymap.set("n", "<leader>gr", "<cmd>GoRun<cr>", { buffer = 0, desc = "Run Go file" })
---
+++ after/ftplugin/gosum.lua
@@ -0,0 +1,6 @@
+-- go.sum specific settings
+vim.opt_local.expandtab = false
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 0
+vim.opt_local.listchars = "tab:  ,extends:›,precedes:‹,nbsp:·,trail:·"
