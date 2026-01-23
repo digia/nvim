5f9b61a583e5778b9d58252b8d62eb50f9a13ef7
digia <jon@digia.io>
2026-01-22 19:02:55 -0500

    feat: add border to `lsp.buf.hover`

--- lua/digia/plugin/lsp.lua
+++ lua/digia/plugin/lsp.lua
@@ -60,6 +60,8 @@ return {
         nmap("<leader>vr", vim.lsp.buf.references)
         nmap("<leader>vh", vim.lsp.buf.signature_help) -- Necessary with `K`?
 
+        nmap("K", function() vim.lsp.buf.hover({ border = "rounded" }) end, "Hover")
+
         nmap("]d", vim.diagnostic.goto_next)
         nmap("[d", vim.diagnostic.goto_prev)
         nmap("]e", function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR }) end, "Next Error")
