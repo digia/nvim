3a300f4fe0ed5936dfbc95e06abf58d5108eec20
digia <jon@digia.io>
2026-05-25 19:27:27 -0400

    fix: <leader>fd prompt title

--- lua/digia/plugin/telescope.lua
+++ lua/digia/plugin/telescope.lua
@@ -37,8 +37,8 @@ return {
         vim.notify("No doc/ or docs/ in " .. root, vim.log.levels.WARN)
         return
       end
-      require("telescope.builtin").find_files({ cwd = dir })
-    end,                                                                              desc = "Find Doc Files" },
+      require("telescope.builtin").find_files({ cwd = dir, prompt_title = "Find Doc Files" })
+    end,                                                                            desc = "Find Doc Files" },
     { "<leader>fr",  "<cmd>Telescope oldfiles<cr>",                                 desc = "Recent Files" }, -- Previously opened files
 
     -- Search Utilities
