cc209f76a13d969b642a60d76fe73978847bd3ed
digia <jon@digia.io>
2026-09-07 19:31:07 -0400

    feat: adjust tag auto closing within TypeScript files

--- lua/digia/plugin/treesitter.lua
+++ lua/digia/plugin/treesitter.lua
@@ -66,6 +66,11 @@ return {
   {
     "windwp/nvim-ts-autotag",
     event = { "BufReadPre", "BufNewFile" },
-    opts = {},
+    opts = {
+      opts = {
+        enable_close = false,
+        enable_close_on_slash = true,
+      },
+    },
   },
 }
